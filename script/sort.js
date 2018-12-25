e187cbe4857acc2ca500088dccace407811c0285
yzhengaq <yzhengaq@gmail.com>
2018-12-25 22:49:44 +0800

    feat: Implementation of insertion, shell and quick sort.

--- script/sort.js
+++ script/sort.js
@@ -1,31 +1,35 @@
 /*
- * @Description: Some example of class sort
+ * @Description: Some example of sort algorithm
  * @Author: Kevin
  * @Date: 2018-12-23 16:28:07
- * @LastEditTime: 2018-12-23 19:48:02
+ * @LastEditTime: 2018-12-25 22:47:35
  */
 
-const Sort = require('../lib/Sort');
 const chalk = require('chalk');
+const Sort = require('../lib/Sort');
+
 const logger = console.log;
 
 
 function runSort(func) {
   logger(chalk.blue(func));
 
-  const line = Array.from({ length: 200 }, () => Math.floor(Math.random() * 10000));
+  const line = Array.from({ length: 10000 }, () => Math.floor(Math.random() * 500000));
 
-  logger('origin Array:', `[${line.join(',')}]`);
+  // logger('origin Array:', `[${line.join(',')}]`);
 
   const sortIns = new Sort();
   const t1 = Date.now();
   const result = sortIns[func](line);
   const t2 = Date.now();
-  
-  logger(`${sortIns.sortType} result:`, `[${result.join(',')}]`);
+
+  // logger(`${sortIns.sortType} result:`, `[${result.join(',')}]`);
   logger(chalk.cyan(`${sortIns.sortType} cost time:`), chalk.green(`${t2 - t1}ms`));
-  logger('\n')
+  return result;
 }
 
 runSort('bubbleSort');
 runSort('selectionSort');
+runSort('insertionSort');
+runSort('shellSort');
+runSort('quickSort');
